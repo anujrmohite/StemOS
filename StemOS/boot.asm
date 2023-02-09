f00fbb999246a78ef483b8f89d2c3038ded43ee7
Anuj R Mohite <40718422+anujrmohite@users.noreply.github.com>
2023-02-09 23:29:39 +0530

    Add files via upload

---
+++ StemOS/boot.asm
@@ -0,0 +1,15 @@
+org 0x7c00
+;bios loading address
+BITS 16
+
+start:
+    mov ah,0eh
+    mov al,'A'
+    mov bx,0
+    int 0x10
+    ;bios is like in kernal itself
+    ;interrupt jump table 
+    jmp $
+
+times 510-($ - $$) db 0
+dw 0xAA55
